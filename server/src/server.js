efa05dc5eb7d26fb9fc5942348347e1579f96f7c
JJ Jensen <jonathanjensenjr@gmail.com>
2025-12-31 15:21:07 -0700

    initial

---
+++ server/src/server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const app = require("./app");
+
+const { loadPlanetsData } = require('./models/planets.model');
+
+const PORT = process.env.PORT || 8000;
+
+async function startServer() {
+  await loadPlanetsData();
+  const server = http.createServer(app);
+  server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}...`);
+  });
+}
+
+startServer();
