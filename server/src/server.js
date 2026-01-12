620ff4a652ffe4603050704e2389951fee8fa2eb
JJ Jensen <jonathanjensenjr@gmail.com>
2026-01-12 12:45:00 -0700

    root gitignore, package updates for mongo, launches schema

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require("./app");
 
@@ -6,9 +7,23 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = "mongodb+srv://jonathanjensenjr_db_user:ig2iVtDBVkilS5UZ@cluster0.gw2b9wa.mongodb.net/nasa?appName=Cluster0";
+
+const server = http.createServer(app);
+
+mongoose.set('strictQuery', true);
+
+mongoose.connection.once('open', () => {
+  console.log('Mongo DB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
-  const server = http.createServer(app);
   server.listen(PORT, () => {
     console.log(`listening on port ${PORT}...`);
   });
